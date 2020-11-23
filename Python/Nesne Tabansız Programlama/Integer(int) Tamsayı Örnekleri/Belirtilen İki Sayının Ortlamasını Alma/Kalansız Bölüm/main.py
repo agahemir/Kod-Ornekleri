fae77f5c5bef9ab7e95d7f83a620341e798bd481
Agah Emir <70206891+agahemir@users.noreply.github.com>
2020-11-23 21:49:09 +0300

    Create main.py

---
+++ Python/Nesne Tabansız Programlama/Integer(int) Tamsayı Örnekleri/Belirtilen İki Sayının Ortlamasını Alma/Kalansız Bölüm/main.py
@@ -0,0 +1,4 @@
+ilkSayi = int(input("İlk Sayıyı Giriniz: ")) #Kullanıcıdan Alınan Girdi "ilkSayi" Değişkenine Atanıyor.
+ikinciSayi = int(input("İkinci Sayıyı Giriniz: ")) #Kullanıcıdan Alınan Girdi "ikinciSayi" Değişkenine Atanıyor.
+cozumSayi = (ilkSayi + ikinciSayi)//2 #"ilkSayı" ve "ikinciSayi" Toplanır Ardından İkiye Kalansız Olarak Bölünüp "cozumSayi" Değişkeninde Tutulur.
+print(cozumSayi) #"cozumSayi" Değişkeni Ekrana Bastırılır.
